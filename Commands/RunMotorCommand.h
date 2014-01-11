637c83ee690e253411cf47a65a23c250e2f74bd9
Evan Kovacs <ekovacs3.15k@gmail.com>
2014-01-11 14:04:25 -0500

    Fixed random u in RunMotorCommand.h

--- Commands/RunMotorCommand.h
+++ Commands/RunMotorCommand.h
@@ -1,1 +1,19 @@
-u
+#ifndef RUN_MOTOR_COMMAND_H
+#define RUN_MOTOR_COMMAND_H
+
+#include "../CommandBase.h"
+
+class RunMotorCommand : public CommandBase {
+public:
+	RunMotorCommand(float speed);
+	virtual void Initialize();
+	virtual void Execute();
+	virtual bool IsFinished();
+	virtual void End();
+	virtual void Interrupted();
+
+protected:
+	float m_speed;
+};
+
+#endif
