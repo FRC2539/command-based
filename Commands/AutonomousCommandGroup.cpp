49e45136364b8848b8ac964a4b39583bdcd55b71
Krypton Cougars <macbook1@kryptoncougars.pasd.us>
2014-01-16 21:05:58 -0500

    makes the motor dance

--- Commands/AutonomousCommandGroup.cpp
+++ Commands/AutonomousCommandGroup.cpp
@@ -1,21 +1,19 @@
 #include "AutonomousCommandGroup.h"
+#include <Commands/WaitCommand.h>
 
 AutonomousCommandGroup::AutonomousCommandGroup() {
-	int count = 5;
-	int i = 0;
-	while (i < count)
+	int start = 0;
+	while (start < 20)
 	{
-		AddSequential(new RunMotorCommand(1));
-		AddSequential(new RunTalonCommand(.5));
-		AddSequential(new WaitCommand(.5));
 		AddSequential(new RunMotorCommand(-1));
-		AddSequential(new RunTalonCommand(-.5));
-		AddSequential(new WaitCommand(.5));
+		AddSequential(new WaitCommand(.1));
+		AddSequential(new StopMotorCommand());
+		AddSequential(new WaitCommand(.1));
 
-		i++;
+		start++;
 	}
-
-	AddSequential(new StopMotorCommand());
+	AddSequential(new RunTalonCommand(.5));
+	AddSequential(new WaitCommand(.5));
 	AddSequential(new StopTalonCommand());
 }
 
