f0a38b4d89ce433f13c9ee1a5fca1a13a7eb46f4
Krypton Cougars <macbook1@kryptoncougars.pasd.us>
2014-02-24 20:38:30 -0500

    Remove wait from ResetCommand

--- Commands/ResetCommand.cpp
+++ Commands/ResetCommand.cpp
@@ -10,15 +10,13 @@ void ResetCommand::Initialize() {
 	drivetrain->move(0, 0);
 	pickup->pickup(0);
 	shooter->reset();
-
-	SetTimeout(0.2);
 }
 
 void ResetCommand::Execute() {
 }
 
 bool ResetCommand::IsFinished() {
-	return IsTimedOut();
+	return true;
 }
 
 void ResetCommand::End() {
