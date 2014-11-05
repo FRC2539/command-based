93bf99d3331cd86c78510b53acd7a7617b8e8220
Krypton Cougars <macbook1@kryptoncougars.pasd.us>
2014-11-04 19:15:21 -0500

    Make StartCompressorCommand Default

--- Commands/StartCompressorCommand.h
+++ Commands/StartCompressorCommand.h
@@ -1,9 +1,14 @@
 #ifndef START_COMPRESSOR_COMMAND_H
 #define START_COMPRESSOR_COMMAND_H
 
-#include "Types/InstantCommand.h"
+#include "Types/DefaultCommand.h"
 
-class StartCompressorCommand : public InstantCommand {
+/* Even though this command could be an InstantCommand, because it is used as
+ * the default command for the AirCompressor subsystem, it is made a
+ * DefaultCommand. An InstantCommand would be restarted constantly.
+ */
+
+class StartCompressorCommand : public DefaultCommand {
 
 public:
 	StartCompressorCommand();
