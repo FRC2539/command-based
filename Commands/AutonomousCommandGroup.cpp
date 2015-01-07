df121d6608dffb5a22e8c67a6577b7b11a16538e
Krypton Cougars <macbook1@kryptoncougars.pasd.us>
2015-01-07 18:31:16 -0500

    Removed motor jumping

--- Commands/AutonomousCommandGroup.cpp
+++ Commands/AutonomousCommandGroup.cpp
@@ -2,18 +2,8 @@
 #include <Commands/WaitCommand.h>
 
 AutonomousCommandGroup::AutonomousCommandGroup() {
-	int start = 0;
-	while (start < 20)
-	{
-		AddSequential(new RunMotorCommand(-1));
-		AddSequential(new WaitCommand(.1));
-		AddSequential(new StopMotorCommand());
-		AddSequential(new WaitCommand(.1));
-
-		start++;
-	}
 	AddSequential(new RunTalonCommand(.5));
-	AddSequential(new WaitCommand(.5));
+	AddSequential(new WaitCommand(2));
 	AddSequential(new StopTalonCommand());
 }
 
