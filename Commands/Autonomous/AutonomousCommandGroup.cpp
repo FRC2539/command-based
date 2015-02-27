c2ef8f64ccf2cc62a7833cf0cc1c381e9557bcd3
Krypton Cougars <latitude1@kryptoncougars.pasd.us>
2015-02-26 20:35:26 -0500

    Changed commented auton

--- Commands/Autonomous/AutonomousCommandGroup.cpp
+++ Commands/Autonomous/AutonomousCommandGroup.cpp
@@ -10,15 +10,15 @@
 AutonomousCommandGroup::AutonomousCommandGroup() : CommandGroup("Autonomous")
 {
 	AddSequential(new ResetCommand());
-// 	AddSequential(new SetTinesCommand(22));
-// 	AddSequential(new SetElevatorCommand(4655));
+	AddSequential(new SetTinesCommand(22));
+	AddSequential(new SetElevatorCommand(4655));
  	AddSequential(new DriveDistanceCommand(22, DriveDistanceCommand::Y));
-// 	AddSequential(new SetElevatorCommand(4100));
+	AddSequential(new SetElevatorCommand(4100));
  	AddSequential(new DriveDistanceCommand(-6, DriveDistanceCommand::Y));
-// 	AddSequential(new SetElevatorCommand(0));
+	AddSequential(new SetElevatorCommand(0));
  	AddSequential(new DriveDistanceCommand(2, DriveDistanceCommand::Y));
-// 	AddSequential(new SetTinesCommand(16));
-// 	AddSequential(new SetElevatorCommand(500));
+	AddSequential(new SetTinesCommand(16));
+	AddSequential(new SetElevatorCommand(500));
 	AddSequential(new TurnAngleCommand(-85)); 
 	AddSequential(new DriveDistanceCommand(115, DriveDistanceCommand::Y, 100));
 
